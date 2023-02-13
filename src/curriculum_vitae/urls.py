d40b55c0afbe614c13bbf5f7bc3d1e5604fd7e7c
Rafal <raf88pogo@gmail.com>
2023-02-13 20:57:31 +0100

    refactor urls

--- src/curriculum_vitae/urls.py
+++ src/curriculum_vitae/urls.py
@@ -14,16 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import jobs.views
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+import jobs.views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", jobs.views.home, name="home"),
-    path("jobs/<int:job_id>", jobs.views.detail, name="detail"),
-
+    path("", include('jobs.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
